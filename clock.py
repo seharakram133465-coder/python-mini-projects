ec6ab6a38a85a82378775c2b061cb73adf0c2e5e
seharakram133465-coder <seharakram133465@gmail.com>
2026-04-23 01:41:14 -0700

    clock project

---
+++ clock.py
@@ -0,0 +1,19 @@
+
+import tkinter as tk
+from time import strftime
+
+root=tk.Tk()
+root.title("digital clock")
+
+def time():
+    string=strftime("%H:%M:%S %p n \n %D")
+    label.config(text=string)
+    label.after(1000,time)
+
+
+label=tk.Label(root,font=('calibri',50,'bold'), background='grey',foreground='black')
+label.pack(anchor="center")
+
+time()
+
+root.mainloop()
